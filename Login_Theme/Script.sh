8b72c84521f5804af6d8dab84fcc8897c3fd4256
Kaio Santos <60193295+KaioSant@users.noreply.github.com>
2021-12-08 21:48:13 -0300

    Update Script.sh

--- Login_Theme/Script.sh
+++ Login_Theme/Script.sh
@@ -1,7 +1,7 @@
 php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/auth
-rm -rf LoginFromContainer.tsx
-wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/login-logo/LoginFromContainer.tsx
+rm -rf LoginContainer.tsx
+wget https://raw.githubusercontent.com/KaioSant/Addons-Pterodactyl-WhiterHosting/main/Login_Theme/LoginContainer.tsx
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt install -y nodejs
 npm i -g yarn
@@ -11,4 +11,5 @@ yarn add @emotion/react
 yarn build:production
 php /var/www/pterodactyl/artisan up
 clear
-echo "Done"
+echo "Script instalado com sucesso !"
+echo "Feito Por Kaio Santos"
